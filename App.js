00ef1d1e0dacca798f3a5329a689ec0fd9e22daa
Anton Honcharov <antonhoncharovv@gmail.com>
2022-10-21 18:45:52 +0300

    add color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
   },
